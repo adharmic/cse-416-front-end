e70370d4a889ee4e96b00db25678c58bfc478b49
Adithya Ajith <50308085+adithyajith925@users.noreply.github.com>
2022-02-26 18:32:56 -0500

    specific state support added

--- leaflet.js
+++ leaflet.js
@@ -1,7 +1,13 @@
 
+const DISTRICTING_STATES = ["Nevada", "Florida"]
+
+const states = state_data.features.filter(function (entry) {
+    return DISTRICTING_STATES.includes(entry.properties.NAME);
+});
+
 var map = L.map('map', {
     minZoom: 4,
-    maxZoom: 5,
+    maxZoom: 10,
     zoom: 4.5,
     maxBounds: L.latLngBounds(L.latLng(24.5, -124.7), L.latLng(49.4, -67)),
     zoomSnap: .5
@@ -12,5 +18,9 @@ var cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyrigh
 var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
     attribution: cartodbAttribution
 }).addTo(map);
+
+L.geoJSON(states).addTo(map);
+
+
 
 map.setView([0, 0], 0);
