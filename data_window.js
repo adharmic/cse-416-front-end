566bbabebb19f9633814b9c09ae0a6b0a3e6193c
LAPTOP-HENSMSN0\adith <adithyaajith@prohashing.com>
2022-03-08 22:38:36 -0500

    added generated plan tab and screenshot button

--- data_window.js
+++ data_window.js
@@ -12,5 +12,20 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  function openPlantab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent1");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks1");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
   
 document.getElementById("defaultOpen").click();
+document.getElementById("defaultPlantabOpen").click();
